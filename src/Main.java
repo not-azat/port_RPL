4674fb592284690b49ec8d04ca4da3bcdfe4ba00
Azat Abdulvaliev <abdulvaliev@mercurydevelopment.com>
2014-05-15 13:31:32 +0400

    refactoring whatever

--- src/Main.java
+++ src/Main.java
@@ -27,6 +27,78 @@ public class Main {
     }
 
 
+    ///////// USER INPUT //////////////
+
+    static abstract class UserInputLoop {
+        void start(OutputStream os) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            sendBytes(getInitCommand(), os);
+            while (true) {
+                System.out.print("> ");
+                String input = br.readLine();
+                sendBytes(getBytesFromInputLine(input), os);
+                System.out.println();
+            }
+        }
+
+        private void sendBytes(byte[] bytes, OutputStream os) throws IOException {
+            os.write(bytes);
+            os.flush();
+        }
+
+        abstract byte[] getBytesFromInputLine(String inputLine);
+        abstract byte[] getInitCommand();
+    }
+
+
+
+    static class ASCIIUserInputLoop extends UserInputLoop {
+        byte[] getBytesFromInputLine(String inputLine) {
+            try {
+                return inputLine.getBytes("US-ASCII");
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+                throw new RuntimeException(e);
+            }
+        }
+
+        @Override
+        byte[] getInitCommand() {
+            return new byte[] {0x1c, 0x3c, 0x53, 0x56, 0x45, 0x4C, 0x3E};
+        }
+    }
+
+
+
+    static class HEXUserInputLoop extends UserInputLoop {
+        byte[] getBytesFromInputLine(String inputLine) {
+            return hexStringToByteArray(inputLine);
+        }
+
+        @Override
+        byte[] getInitCommand() {
+            try {
+                return "<EPOS>".getBytes("US-ASCII");
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+                throw new RuntimeException(e);
+            }
+        }
+
+        public static byte[] hexStringToByteArray(String s) {
+            int len = s.length();
+            byte[] data = new byte[len / 2];
+            for (int i = 0; i < len; i += 2) {
+                data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4) + Character.digit(s.charAt(i+1), 16));
+            }
+            return data;
+        }
+    }
+
+
+    ///////////// PORT FEEDBACK //////////////////
+
+
     static  abstract class PortFeedbackLoop {
         public void start(final InputStream is) {
             Thread t = new Thread(new Runnable() {
@@ -77,70 +149,6 @@ public class Main {
         abstract String interpretFeedbackBytes(char[] bytes);
     }
 
-
-    static abstract class UserInputLoop {
-        void start(OutputStream os) throws IOException {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            sendBytes(getInitCommand(), os);
-            while (true) {
-                System.out.print("> ");
-                String input = br.readLine();
-                sendBytes(getBytesFromInputLine(input), os);
-                System.out.println();
-            }
-        }
-
-        private void sendBytes(byte[] bytes, OutputStream os) throws IOException {
-            os.write(bytes);
-            os.flush();
-        }
-
-        abstract byte[] getBytesFromInputLine(String inputLine);
-        abstract byte[] getInitCommand();
-    }
-
-
-    static class ASCIIUserInputLoop extends UserInputLoop {
-        byte[] getBytesFromInputLine(String inputLine) {
-            try {
-                return inputLine.getBytes("US-ASCII");
-            } catch (UnsupportedEncodingException e) {
-                e.printStackTrace();
-                throw new RuntimeException(e);
-            }
-        }
-
-        @Override
-        byte[] getInitCommand() {
-            return new byte[] {0x1c, 0x3c, 0x53, 0x56, 0x45, 0x4C, 0x3E};
-        }
-    }
-
-
-    static class HEXUserInputLoop extends UserInputLoop {
-        byte[] getBytesFromInputLine(String inputLine) {
-            return hexStringToByteArray(inputLine);
-        }
-
-        @Override
-        byte[] getInitCommand() {
-            try {
-                return "<EPOS>".getBytes("US-ASCII");
-            } catch (UnsupportedEncodingException e) {
-                e.printStackTrace();
-                throw new RuntimeException(e);
-            }
-        }
-
-        public static byte[] hexStringToByteArray(String s) {
-            int len = s.length();
-            byte[] data = new byte[len / 2];
-            for (int i = 0; i < len; i += 2) {
-                data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4) + Character.digit(s.charAt(i+1), 16));
-            }
-            return data;
-        }
-    }
 
 
     static class DefaultPortFeedbackLoop extends PortFeedbackLoop {
